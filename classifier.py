7194ddd25ff84b5ec20fb867377fea72f212943e
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-10-24 15:42:25 +0100

    changes corpus processing

--- classifier.py
+++ classifier.py
@@ -1,36 +1,35 @@
 import nltk
-from nltk.stem.lancaster import LancasterStemmer
-nltk.download('punkt')
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB
 
-stemmer = LancasterStemmer()
-
-#processamento do corpus
+#processamento do ficheiro
 training_data = []
 corpus =  open('corpora/QuestoesConhecidas.txt', 'r')
 with open('corpora/QuestoesConhecidas.txt') as corpus:
     for line in corpus:
         (firstWord, rest) = line.split(maxsplit = 1)
-        training_data.append({"class": firstWord,"sentence": rest.rstrip()})
+        training_data.append({"category": firstWord,"sentence": rest.rstrip()})
 
-corpus_words = {}
-class_words = {}
 
-classes = list(set([a['class'] for a in training_data]))
+categories = list(set([a['category'] for a in training_data]))
+class_words = []
+corpus_words = []
 
-for c in classes:
-    class_words[c] = []
+    
+for category in categories:
+    words=[]
+    for line in training_data:
+        for word in nltk.word_tokenize(line['sentence']):
+            if word not in ('?', ':', '.', ',' "'s"):
+                corpus_words.append(word.lower())
+                if line['category'] == category:
+                    words.append(word.lower())
+    class_words.append((words, category))
 
-for line in training_data:
-    for word in nltk.word_tokenize(line['sentence']):
-        if word not in ["?", "'s"]:
-            stemmed_word = stemmer.stem(word.lower())
-            if stemmed_word not in corpus_words:
-                corpus_words[stemmed_word] = 1
-            else:
-                corpus_words[stemmed_word] += 1
-            class_words[line['class']].extend([stemmed_word])
+corpus_words = nltk.FreqDist(corpus_words)
 
-# we now have each stemmed word and the number of occurances of the word in our training corpus (the word's commonality)
-print ("Corpus words and counts: %s \n" % corpus_words)
-# also we have all words in each class
-print ("Class words: %s" % class_words) 
+#def create_training_set(class_words):
+
+#MNB_classifier = SklearnClassifier(MultinomialNB())
+#MNB_classifier.train(class_words)
+#print("MultinomialNB accuracy percent:",nltk.classify.accuracy(MNB_classifier, class_words))
